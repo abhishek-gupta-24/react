7bee0539e57933c3905dc461f23f6a976b36e62b
Abhishek Gupta <abhishek.gupta.150803@gmail.com>
2025-03-31 17:22:24 +0530

    todoProject

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -3,8 +3,8 @@ import { useState, useContext } from "react";
 import UserContext from "../context/UserContext";
 
 function Login(){
-    const [username,setUsername]=useState('');
-    const [password,setPassword]=useState('');
+    const [username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
     const {setUser}=useContext(UserContext)
     const handleSubmit=(e)=>{
         e.preventDefault()
